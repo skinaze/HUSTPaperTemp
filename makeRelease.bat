c12ff78b6cb3cf8f627c76491ca6f1e6f4113e02
Skinaze <me@stringblog.com>
2017-03-23 17:26:55 +0800

    Finish makeRelease.bat. Fix version bug of ctex.

--- makeRelease.bat
+++ makeRelease.bat
@@ -1,4 +1,6 @@
 @echo off
+echo Please input release version:
+set /P version=
 echo Start to Copy the Example......
 copy "Example\example.tex" "Release\Example\"
 copy "Example\example.pdf" "Release\Example\"
@@ -9,16 +11,19 @@ copy "Example\gbt7714-2005.bst" "Release\Example\"
 copy "Example\HUSTBlack.png" "Release\Example\"
 copy "Example\HUSTGreen.png" "Release\Example\"
 echo Example Copied! 
-
+echo.
 echo Start to Copy the Template......
 copy "Example\HustGraduPaper.cls" "Release\Template\"
 copy "Example\gbt7714-2005.bst" "Release\Template\"
 copy "Example\HUSTBlack.png" "Release\Template\"
 copy "Example\HUSTGreen.png" "Release\Template\"
 echo Template Copied!
-
+echo.
 echo Start to Copy the Instruction......
 copy "Instruction\HGP.pdf" "Release\使用说明.pdf"
 echo Instruction Copied
+echo.
+echo Compressing......
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('Release', 'Version\HustGraduPaper.%version%.zip'); }"
 
 echo ALL DONE!
