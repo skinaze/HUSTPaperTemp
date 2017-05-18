7aaaa053af2e28ebbad65023bd49100aafc6a318
Skinaze <me@stringblog.com>
2017-05-18 17:29:36 +0800

    Change logo to vector graphics.

--- makeRelease.bat
+++ makeRelease.bat
@@ -8,15 +8,15 @@ copy "Example\Bibs\*.bib" "Release\Example\Bibs\"
 copy "Example\Figures\*" "Release\Example\Figures\"
 copy "Example\HustGraduPaper.cls" "Release\Example\"
 copy "Example\gbt7714-2005.bst" "Release\Example\"
-copy "Example\HUSTBlack.png" "Release\Example\"
-copy "Example\HUSTGreen.png" "Release\Example\"
+copy "Example\HUSTBlack.eps" "Release\Example\"
+copy "Example\HUSTGreen.eps" "Release\Example\"
 echo Example Copied! 
 echo.
 echo Start to Copy the Template......
 copy "Example\HustGraduPaper.cls" "Release\Template\"
 copy "Example\gbt7714-2005.bst" "Release\Template\"
-copy "Example\HUSTBlack.png" "Release\Template\"
-copy "Example\HUSTGreen.png" "Release\Template\"
+copy "Example\HUSTBlack.eps" "Release\Template\"
+copy "Example\HUSTGreen.eps" "Release\Template\"
 echo Template Copied!
 echo.
 echo Start to Copy the Instruction......
